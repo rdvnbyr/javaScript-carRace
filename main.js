adfd1c48b0489a991899eb314bcbeeb35165c45f
rdvnbyr <57366987+rdvnbyr@users.noreply.github.com>
2020-01-16 18:50:42 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let startGame = () => {
     var canv = document.getElementById("myCanvas");
     var contex = canv.getContext("2d");
     contex.fillStyle = "black"
-    contex.fillRect(500, 0 ,1 ,200 )
+    contex.fillRect(750, 0 ,1 ,200 )
 
     var imgCar = document.getElementById("Vosvos");
     var imgTax = document.getElementById("taxi");
@@ -18,19 +18,19 @@ let startGame = () => {
         posVosvos += moveVos;
         posTaxi += moveTax;
 
-        if (posVosvos > 500) {
+        if (posVosvos > 700) {
             moveVos = 0;
             moveTax = 0;
             document.getElementById("text_p").innerHTML = `Vosvos kazandi.`
         }
 
-        if (posTaxi > 500) {
+        if (posTaxi > 700) {
             moveVos = 0;
             moveTax = 0;
             document.getElementById("text_p").innerHTML = `Taxi kazandi.`
         }
 
-        if (posTaxi > 500 && posVosvos > 500) {
+        if (posTaxi > 700 && posVosvos > 700) {
             moveVos = 0;
             moveTax = 0;
             document.getElementById("text_p").innerHTML = `Beraberlik.`
